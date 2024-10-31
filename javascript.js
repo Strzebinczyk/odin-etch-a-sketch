478ee329443a33cef469ef21c5d08b125f7c2246
Strzebinczyk <kar.strzebinczyk@gmail.com>
2024-10-31 14:09:45 +0100

    Add grid cells toggle dark class on hover

--- javascript.js
+++ javascript.js
@@ -1,10 +1,22 @@
+//Set up a grid
 for (let i = 0; i < 16; i++) {
   let container = document.createElement("div");
   for (let j = 0; j < 16; j++){
     let div = document.createElement("div");
-    div.classList += "cell";
+    div.classList.toggle("cell");
     container.appendChild(div);
   }
   let grid = document.querySelector("#grid");
   grid.appendChild(container);
 }
+
+function darken(element) {
+  console.log(element.target);
+}
+
+gridCells = document.querySelectorAll(".cell")
+gridCells.forEach((cell) => {
+  cell.addEventListener("mouseover", function (e) {
+    e.target.classList.toggle("dark");
+  });
+});
